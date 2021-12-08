eedc1e4210b46b615d180d226a968657ec89383e
plinnlatt <plinnlatt70@gmail.com>
2021-12-09 01:19:14 +0630

    Initialize the amzone starter package

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Amazone.2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hello</h1>
+
+    </div>
+  )
+}
