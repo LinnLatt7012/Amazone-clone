15ad227d377bd26e2ad0a5e6f35b66c000e573cf
plinnlatt <plinnlatt70@gmail.com>
2021-12-11 15:08:27 +0630

    Next-auth version change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
 import { SessionProvider } from 'next-auth/react';
 import Header from '../components/Header';
 import '../styles/global.css'
-
+// import {Provider as AuthProvider} from "next-auth/client"
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps} }) => {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider  session={session}>
       <Header />
       <Component {...pageProps} />
     </SessionProvider>
